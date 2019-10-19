d75a129ca616d8bbbfd3edc444265f288104150c
Jamaal Ahmed <jamaal729@yahoo.com>
2019-10-19 17:35:54 +0000

    Implemented sum array of numbers

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+
+  let sum = 0;
+  for (item of array) {
+
+    if (!Array.isArray(item)) {
+      sum += item;
+    } else {
+      sum += sumItems(item);
+    }
+    // console.log(sum);
+  }
+  return sum;
 }
 
 module.exports = sumItems;
+
+console.log(sumItems([1, 2, 3, 4, 5]));
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
+console.log(sumItems([[[[[[[[[[[[[1]]]]]]]]]]]]]));
+
+// function printItemsRecursive(array) {
+//   for (item of array) {
+//     if (!Array.isArray(item)) {
+//       console.log(item);
+//     } else {
+//       printItemsRecursive(item);
+//     }
+//   }
+// }
